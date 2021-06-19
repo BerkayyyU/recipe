1cf6db4002183b3bd0f884f7950422bed2f34fc4
Berkay Ulguel <berkayyyulguel@gmail.com>
2021-06-19 19:36:56 +0300

    bootstrap implemented

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO category (description) VALUES ('American');
+/*INSERT INTO category (description) VALUES ('American');
 
-INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');*/
