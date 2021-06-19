04ee660fd1f1e582c0dd8952a9ca61c85472fd1b
Berkay Ulguel <berkayyyulguel@gmail.com>
2021-06-19 17:52:35 +0300

    Deleted getter setters due to lombok implementation(@data)

--- src/main/java/tr/edu/trakya/berkayulguel/recipe/models/Category.java
+++ src/main/java/tr/edu/trakya/berkayulguel/recipe/models/Category.java
@@ -1,9 +1,12 @@
 package tr.edu.trakya.berkayulguel.recipe.models;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
+@Data
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,27 +16,5 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
